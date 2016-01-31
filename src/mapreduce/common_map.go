8d3b0d6b97119855d2dd1351b8d81445a880b2b5
Daniel Ziegler <dmz@mit.edu>
2016-01-30 23:51:42 -0500

    Some more clarification
    The paper talks about MapReduce *jobs* but reduce and map *tasks*.

--- src/mapreduce/common_map.go
+++ src/mapreduce/common_map.go
@@ -6,23 +6,23 @@ import (
 
 // doMap does the job of a map worker: it reads the one of the input files
 // (inFile), calls the user-defined map function (mapF) for that file's
-// contents, and writes the output into nReduce intermediate bins.
+// contents, and partitions the output into nReduce intermediate files.
 func doMap(
-	jobName string,
-	job int, // which map job this is
+	jobName string, // the name of the whole MapReduce job
+	mapTaskNumber int, // which map task this is
 	inFile string,
-	nReduce int, // the number of reduce jobs that will be run
+	nReduce int, // the number of reduce task that will be run ("R" in the paper)
 	mapF func(file string, contents string) []KeyValue,
 ) {
 	// TODO:
 	// You will need to write this function.
-	// You can find the intermediate bin filename for reduce job r using
-	// reduceName(jobName, job, r). The ihash function (given below doMap) should
-	// be used to decide which bin a given key belongs into.
+	// You can find the filename for this map task's input to reduce task number
+	// r using reduceName(jobName, mapTaskNumber, r). The ihash function (given
+	// below doMap) should be used to decide which file a given key belongs into.
 	//
 	// You may choose how you would like to encode the key/value pairs in
 	// the intermediate files. One way would be to encode them using JSON.
-	// Since the reduce jobs have to output JSON anyway, you should
+	// Since the reduce tasks have to output JSON anyway, you should
 	// probably do that here too. You can write JSON to a file using
 	//
 	//   enc := json.NewEncoder(file)
--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,31 +1,30 @@
 package mapreduce
 
 // doReduce does the job of a reduce worker: it reads the intermediate
-// key/value pairs (produced by the map phase) for the bin assigned to this job
-// (jobName), sorts the intermediate key/value pairs by key, calls the
-// user-defined reduce function (reduceF) for each key, and writes the output
-// to disk.
+// key/value pairs (produced by the map phase) for this job, sorts the
+// intermediate key/value pairs by key, calls the user-defined reduce function
+// (reduceF) for each key, and writes the output to disk.
 func doReduce(
-	jobName string,
-	job int, // which reduce job this is
-	nMap int, // the number of map jobs that were run
+	jobName string, // the name of the whole MapReduce job
+	reduceTaskNumber int, // which reduce task this is
+	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
 	// TODO:
 	// You will need to write this function.
-	// You can find the intermediate bin for this reduce job from map job m using
-	// reduceName(jobName, m, job).
+	// You can find the intermediate file for this reduce task from map task number
+	// m using reduceName(jobName, m, reduceTaskNumber).
 	// Remember that you've encoded the values in the intermediate files, so you
 	// will need to decode them. If you chose to use JSON, you can read out
 	// multiple decoded values by creating a decoder, and then repeatedly calling
 	// .Decode() on it until Decode() returns an error.
 	//
 	// You should write the reduced output in as JSON encoded KeyValue
-	// objects to a file named mergeName(jobName, job). We require you to
-	// use JSON here because that is what the merger than combines the
-	// output from all the reduce jobs expects. There is nothing "special"
-	// about JSON -- it is just the marshalling format we chose to use. It
-	// will look something like this:
+	// objects to a file named mergeName(jobName, reduceTaskNumber). We require
+	// you to use JSON here because that is what the merger than combines the
+	// output from all the reduce jobs expects. There is nothing "special" about
+	// JSON -- it is just the marshalling format we chose to use. It will look
+	// something like this:
 	//
 	// enc := json.NewEncoder(mergeFile)
 	// for key in ... {
