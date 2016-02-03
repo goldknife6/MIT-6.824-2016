b0dbab1e3c0491794a7b7ba45b4be323f934ff5b
Jon Gjengset <jon@thesquareplanet.com>
2016-02-03 11:15:22 -0500

    update

--- src/main/test-mr.sh
+++ src/main/test-mr.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-here=$(readlink -e "$(dirname "$0")")
+#!/bin/bash
+here=$(dirname "$0")
+[[ "$here" = /* ]] || here="$PWD/$here"
 export GOPATH="$here/../../"
 echo ""
 echo "==> Part I"
